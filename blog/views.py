de3539d06d256b163c0d963d81e89a75ae13aa4e
Tianzi Cui <cuitianzi0@gmail.com>
2024-04-30 23:49:21 -0600

    view has been set up with return request ><

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+# view is the place to put the "logic" of application
 # Create your views here.
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
